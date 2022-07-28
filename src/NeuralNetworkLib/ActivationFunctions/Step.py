ff22fd3981af95e2446f52f2c49cdc1fa51ed32e
SgtSteel <ivanomatrisciano@gmail.com>
2022-07-28 14:17:15 +0200

    Removed heaviside activation function

--- src/NeuralNetworkLib/ActivationFunctions/Step.py
+++
@@ -1,9 +0,0 @@
-from NeuralNetworkLib.ActivationFunctions.IFunction import IFunction
-
-
-class Step(IFunction):
-    def calculate(x):
-        if x >= 0:
-            return 1
-        else:
-            return 0
