578c8de1f268e0ccc99086755523222e2049461d
SgtSteel <ivanomatrisciano@gmail.com>
2022-07-31 22:25:17 +0200

    Use numpy.heaviside in ReLU

--- src/NeuralNetworkLib/ActivationFunctions/ReLU.py
+++ src/NeuralNetworkLib/ActivationFunctions/ReLU.py
@@ -4,7 +4,7 @@ from NeuralNetworkLib.ActivationFunctions.IFunction import IFunction
 
 class ReLU(IFunction):
     def calculate(x):
-        return x*(np.sign(x) + 1.0) / 2.0 #max(0, x) = x*heaviside(x)
+        return x*np.heaviside(x) #max(0, x) = x*heaviside(x) = x*(np.sign(x) + 1.0) / 2.0
 
     def derivative(x):
-        return (np.sign(x) + 1.0) / 2.0 # heaviside
+        return np.heaviside(x) #(np.sign(x) + 1.0) / 2.0 # heaviside
