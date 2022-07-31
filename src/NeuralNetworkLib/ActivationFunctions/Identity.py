fix Identity derivative

src/NeuralNetworkLib/ActivationFunctions/Identity.py
@@ -1,9 +1,9 @@
 from NeuralNetworkLib.ActivationFunctions.IFunction import IFunction
-
+import numpy as np
 
 class Identity(IFunction):
     def calculate(x):
         return x
 
     def derivative(x):
-        return 1
+        return np.ones(len(x))